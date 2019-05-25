7276c833cffafa909b496bd3eeeb31d72e1f52cd
Eric Martin <rip7hockey8@gmail.com>
2019-05-25 18:03:32 +0200

    look & feel finished

--- src/app/issue.ts
+++ src/app/issue.ts
@@ -16,7 +16,7 @@ export class Issue {
         id: number;
         name: string;
         email: string;
-        links: {
+        _links: {
             image: {
                 href: string;
             };
